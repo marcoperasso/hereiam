de5a3ddaba8f51b82f622741bcbc4a4384d2cfb6
Marco Perasso <marco.perasso@gmail.com>
2014-10-30 09:55:16 +0100

    chiedo di attivare il gps

--- src/smartpointer/hereiam/AcceptConnectionActivity.java
+++ src/smartpointer/hereiam/AcceptConnectionActivity.java
@@ -1,14 +1,20 @@
 package smartpointer.hereiam;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class AcceptConnectionActivity extends Activity implements
 		OnClickListener {
 
+	private static final int ACTIVATE_GPS_RESULT = 1;
 	private User user;
 
 	@Override
@@ -30,7 +36,8 @@ public class AcceptConnectionActivity extends Activity implements
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.buttonYes:
-			acceptToSendMyPosition();
+			if (hasGPSOrDontWantToUseIt())
+				acceptToSendMyPosition();
 			break;
 		case R.id.buttonAlways:
 			// prendo l'equivalente utente nella lista, così non devo
@@ -41,7 +48,8 @@ public class AcceptConnectionActivity extends Activity implements
 			fromPhone.trusted = true;
 			fromPhone.saveToDb();
 
-			acceptToSendMyPosition();
+			if (hasGPSOrDontWantToUseIt())
+				acceptToSendMyPosition();
 			break;
 		case R.id.buttonNo:
 			rejectUser();
@@ -50,10 +58,37 @@ public class AcceptConnectionActivity extends Activity implements
 
 	}
 
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == ACTIVATE_GPS_RESULT) {
+			acceptToSendMyPosition();
+		}
+
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+
+	private boolean hasGPSOrDontWantToUseIt() {
+		LocationManager locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		if (locManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+			return true;
+		}
+		Helper.hideableQuestion(this, new IFinishCallback() {
+			@Override
+			public void finished() {
+				Intent myIntent = new Intent(
+						Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+				startActivityForResult(myIntent, ACTIVATE_GPS_RESULT);
+
+			}
+		}, null, R.string.want_gps);
+		return false;
+	}
+
 	private void acceptToSendMyPosition() {
 		MyApplication.getInstance().respondToUser(user.phone,
 				Const.MSG_ACCEPT_CONTACT);
-		ConnectorService.activate(AcceptConnectionActivity.this, user, true, CommandType.START_SENDING_MY_POSITION);
+		ConnectorService.activate(AcceptConnectionActivity.this, user, true,
+				CommandType.START_SENDING_MY_POSITION);
 		finish();
 
 	}
